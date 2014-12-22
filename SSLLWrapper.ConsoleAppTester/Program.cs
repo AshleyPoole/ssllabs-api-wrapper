59b4fe7d91bac76eda3fc8ee4926c6d5133661f0
AshleyPoole <git@ashleypoole.co.uk>
2014-12-22 23:15:45 +0000

    Adding Tests and ConsoleAppTester structural projects

---
+++ SSLLWrapper.ConsoleAppTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SSLLWrapper.ConsoleAppTester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+
+		}
+
+		static void AnalyzeTester()
+		{
+			var apiService = new ApiService("https://api.dev.ssllabs.com/api/fa78d5a4/");
+			var analyze = apiService.Analyze("www.ashleypoole.co.uk");
+
+			Console.WriteLine(string.Format("Has Error Occoured: {0}", analyze.HasErrorOccurred));
+			Console.WriteLine(string.Format("First Error Message: {0}", analyze.Errors.First().message));
+		}
+	}
+}
