34378dfc002e78e493f35fd1892333278a947133
AshleyPoole <git@ashleypoole.co.uk>
2014-12-23 21:44:04 +0000

    Adding more to ConsoleAppTester though not complete

--- SSLLWrapper.ConsoleAppTester/Program.cs
+++ SSLLWrapper.ConsoleAppTester/Program.cs
@@ -5,20 +5,24 @@ namespace SSLLWrapper.ConsoleAppTester
 {
 	class Program
 	{
+		private const string apiUrl = "https://api.dev.ssllabs.com/api/fa78d5a4";
+
 		static void Main(string[] args)
 		{
-			
-
-
+			AnalyzeTester();
 		}
 
 		static void AnalyzeTester()
 		{
-			var apiService = new ApiService("https://api.dev.ssllabs.com/api/fa78d5a4/");
-			var analyze = apiService.Analyze("www.ashleypoole.co.uk");
+			var apiService = new ApiService(apiUrl);
+			var analyze = apiService.Analyze("http://www.ashleypoole.co.uk");
 
-			Console.WriteLine(string.Format("Has Error Occoured: {0}", analyze.HasErrorOccurred));
-			Console.WriteLine(string.Format("First Error Message: {0}", analyze.Errors.First().message));
+			Console.WriteLine("Has Error Occoured: {0}", analyze.HasErrorOccurred);
+			Console.WriteLine("First Error Message: {0}", analyze.Errors.First().message);
+			Console.WriteLine("Status Code: {0}", analyze.Headers.statusCode);
+			Console.WriteLine("Status: {0}", analyze.status);
+
+			Console.ReadLine();
 		}
 	}
 }
