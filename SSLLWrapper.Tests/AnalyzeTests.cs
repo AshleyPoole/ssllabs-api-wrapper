99f6873e8e5939f09d198d17501d601f1702c473
AshleyPoole <git@ashleypoole.co.uk>
2015-01-26 20:56:58 +0000

    Adding unit tests around Info() and few around Analyze()

---
+++ SSLLWrapper.Tests/AnalyzeTests.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Net;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SSLLWrapper;
+using SSLLWrapper.Interfaces;
+using SSLLWrapper.Models;
+using SSLLWrapper.Models.Response;
+
+namespace given_that_I_make_a_analyze_request
+{
+	[TestClass]
+	public class when_the_api_is_online_and_a_valid_request_is_made
+	{
+		private static Analyze _analyzeResponse;
+		private const string TestHost = "https://www.ashleypoole.co.uk";
+
+		[ClassInitialize]
+		public static void Setup(TestContext testContext)
+		{
+			var mockedApiProvider = new Mock<IApiProvider>();
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = "{\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTPS\",\"isPublic\":true,\"status\":\"READY\",\"" +
+				          "startTime\":1422115006431,\"testTime\":1422115131804,\"engineVersion\":\"1.12.8\",\"criteriaVersion\":\"2009i\",\"" +
+				          "endpoints\":[{\"ipAddress\":\"104.28.6.2\",\"statusMessage\":\"Ready\",\"grade\":\"A\",\"hasWarnings\":false,\"" +
+				          "isExceptional\":false,\"progress\":100,\"duration\":64286,\"eta\":2393,\"delegation\":3},{\"ipAddress\":\"104.28.7.2\"" +
+				          ",\"statusMessage\":\"Ready\",\"grade\":\"A\",\"hasWarnings\":false,\"isExceptional\":false,\"progress\":100,\"duration\"" +
+				          ":61046,\"eta\":2393,\"delegation\":3}]}",
+				StatusCode = 200,
+				StatusDescription = "Ok",
+				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost)
+			};
+
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
+
+			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
+			_analyzeResponse = ssllService.Analyze(TestHost);
+		}
+
+		[TestMethod]
+		public void then_the_error_count_should_be_zero()
+		{
+			_analyzeResponse.Errors.Count.Should().Be(0);
+		}
+
+		[TestMethod]
+		public void then_HasErrorOccurred_should_be_false()
+		{
+			_analyzeResponse.HasErrorOccurred.Should().BeFalse();
+		}
+
+		[TestMethod]
+		public void then_the_header_status_code_should_be_200()
+		{
+			_analyzeResponse.Header.statusCode.Should().Be(200);
+		}
+
+		[TestMethod]
+		public void then_the_host_should_match_the_host_passed_into_analyze()
+		{
+			_analyzeResponse.host.Should().Be(TestHost);
+		}
+	}
+}
--- SSLLWrapper.Tests/InfoTests.cs
+++ SSLLWrapper.Tests/InfoTests.cs
@@ -3,12 +3,12 @@ using System.Net;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using SSLLWrapper;
 using SSLLWrapper.Interfaces;
 using SSLLWrapper.Models;
 using SSLLWrapper.Models.Response;
 
-namespace SSLLWrapper.Tests
-//namespace given_that_I_make_a_info_request
+namespace given_that_I_make_a_info_request
 {
 	[TestClass]
 	public class when_the_api_is_online
@@ -18,24 +18,31 @@ namespace SSLLWrapper.Tests
 		[ClassInitialize]
 		public static void Setup(TestContext testContext)
 		{
-			Stream responseStream = new MemoryStream();
 			var mockedApiProvider = new Mock<IApiProvider>();
-			var mockedHttpWebResponse = new Mock<HttpWebResponse>();
-			var streamWriter = new StreamWriter(responseStream);
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = "{\"engineVersion\":\"1.11.4\",\"criteriaVersion\":\"2009i\",\"clientMaxAssessments\":5,\"notice\":\"Some notice goes here\"}",
+				StatusCode = 200,
+				StatusDescription = "Ok",
+				Url = "https://api.dev.ssllabs.com/api/fa78d5a4/info"
+			};
 
-			streamWriter.Write("{\"engineVersion\":\"1.11.4\",\"criteriaVersion\":\"2009i\",\"clientMaxAssessments\":5,\"notice\":\"Some notice goes here\"}");
-
-			mockedHttpWebResponse.Setup(x => x.GetResponseStream()).Returns(responseStream);
-			mockedApiProvider.Setup(x => x.MakeGetRequest(new RequestModel())).Returns(mockedHttpWebResponse.Object);
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
 
 			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
 			_infoResponse = ssllService.Info();
 		}
 
 		[TestMethod]
-		public void then_no_errors_should_be_created()
+		public void then_the_error_count_should_be_zero()
 		{
 			_infoResponse.Errors.Count.Should().Be(0);
+		}
+
+		[TestMethod]
+		public void then_the_HasErrorOccurred_should_be_false()
+		{
+			_infoResponse.HasErrorOccurred.Should().BeFalse();
 		}
 
 		[TestMethod]
@@ -48,6 +55,60 @@ namespace SSLLWrapper.Tests
 		public void then_the_info_response_should_be_populated_with_a_engine_number()
 		{
 			_infoResponse.engineVersion.Should().NotBeNullOrEmpty();
+		}
+
+		[TestMethod]
+		public void then_the_info_response_header_status_code_should_be_200()
+		{
+			_infoResponse.Header.statusCode.Should().Be(200);
+		}
+	}
+
+	[TestClass]
+	public class when_the_api_is_offline
+	{
+		private static Info _infoResponse;
+
+		[ClassInitialize]
+		public static void Setup(TestContext testContext)
+		{
+			var mockedApiProvider = new Mock<IApiProvider>();
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = "",
+				StatusCode = 400,
+				StatusDescription = "Bad Request",
+				Url = "https://api.dev.ssllabs.com/api/fa78d5a4/info"
+			};
+
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
+
+			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
+			_infoResponse = ssllService.Info();
+		}
+
+		[TestMethod]
+		public void then_the_error_count_should_be_zero()
+		{
+			_infoResponse.Errors.Count.Should().BeGreaterOrEqualTo(1);
+		}
+
+		[TestMethod]
+		public void then_the_HasErrorOccurred_should_be_true()
+		{
+			_infoResponse.HasErrorOccurred.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void then_the_api_should_be_marked_as_offline()
+		{
+			_infoResponse.Online.Should().BeFalse();
+		}
+
+		[TestMethod]
+		public void then_the_info_response_header_status_code_should_be_400()
+		{
+			_infoResponse.Header.statusCode.Should().Be(400);
 		}
 	}
 }
