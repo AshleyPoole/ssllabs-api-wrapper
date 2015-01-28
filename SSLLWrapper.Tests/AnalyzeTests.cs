f24015722f2b594984240220463c963ca788f0fd
AshleyPoole <git@ashleypoole.co.uk>
2015-01-28 21:16:29 +0000

    Adding positive Analyze tests

--- SSLLWrapper.Tests/AnalyzeTests.cs
+++ SSLLWrapper.Tests/AnalyzeTests.cs
@@ -1,24 +1,22 @@
-﻿using System.IO;
-using System.Net;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SSLLWrapper;
 using SSLLWrapper.Interfaces;
 using SSLLWrapper.Models;
 using SSLLWrapper.Models.Response;
-using SSLLWrapper.Tests.AnalyzeSharedTests;
+using SSLLWrapper.Tests;
 
 namespace given_that_I_make_a_analyze_request
 {
 	[TestClass]
-	public class when_the_api_is_online_and_a_valid_request_is_made : PositiveTests
+	public class when_a_valid_request_is_made_with_just_a_hostname_and_the_scan_has_completed : PositiveTests
 	{
 		[ClassInitialize]
 		public static void Setup(TestContext testContext)
 		{
 			var mockedApiProvider = new Mock<IApiProvider>();
-			TestHost = "www.ashleypoole.co.uk";
+			TestHost = "https://www.ashleypoole.co.uk";
 			var webResponseModel = new WebResponseModel()
 			{
 				Payloay = "{\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTPS\",\"isPublic\":true,\"status\":\"READY\",\"" +
@@ -35,13 +33,105 @@ namespace given_that_I_make_a_analyze_request
 			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
 
 			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
-			AnalyzeResponse = ssllService.Analyze(TestHost);
+			Response = ssllService.Analyze(TestHost);
+		}
+
+		[TestMethod]
+		public void then_the_scan_results_should_not_be_public()
+		{
+			Response.isPublic.Should().BeFalse();
+		}
+	}
+
+	[TestClass]
+	public class when_a_valid_request_is_made_with_just_a_hostname_and_the_scan_is_at_resolving_stage: PositiveTests
+	{
+		[ClassInitialize]
+		public static void Setup(TestContext testContext)
+		{
+			var mockedApiProvider = new Mock<IApiProvider>();
+			TestHost = "https://www.ashleypoole.co.uk";
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = "{\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTP\",\"isPublic\":false,\"status\":\"DNS\"" +
+				          ",\"statusMessage\":\"Resolving domain names\",\"startTime\":1422475200798,\"engineVersion\":\"1.12.8\"," +
+				          "\"criteriaVersion\":\"2009i\"}",
+				StatusCode = 200,
+				StatusDescription = "Ok",
+				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost)
+			};
+
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
+
+			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
+			Response = ssllService.Analyze(TestHost);
+		}
+
+		[TestMethod]
+		public void then_the_scan_results_should_not_be_public()
+		{
+			Response.isPublic.Should().BeFalse();
+		}
+	}
+
+	[TestClass]
+	public class when_a_valid_request_is_made_with_all_the_inputs_and_the_scan_is_at_endpoint_scanning_stage : PositiveTests
+	{
+		[ClassInitialize]
+		public static void Setup(TestContext testContext)
+		{
+			var mockedApiProvider = new Mock<IApiProvider>();
+			TestHost = "https://www.ashleypoole.co.uk";
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = " {\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTP\",\"isPublic\":false,\"" +
+				          "status\":\"IN_PROGRESS\",\"startTime\":1422479488403,\"engineVersion\":\"1.12.8\",\"criteriaVersion\":\"2009i\"" +
+				          ",\"endpoints\":[{\"ipAddress\":\"104.28.6.2\",\"statusMessage\":\"In progress\",\"statusDetails\":\"TESTING_HTTPS\"" +
+				          ",\"statusDetailsMessage\":\"Sending one complete HTTPS request\",\"progress\":-1,\"eta\":-1,\"delegation\":3}," +
+				          "{\"ipAddress\":\"104.28.7.2\",\"statusMessage\":\"Pending\",\"progress\":-1,\"eta\":-1,\"delegation\":3}]}",
+				StatusCode = 200,
+				StatusDescription = "Ok",
+				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost)
+			};
+
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
+
+			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
+			Response = ssllService.Analyze(TestHost, SSLLService.Publish.On, SSLLService.ClearCache.On,
+				SSLLService.FromCache.Ignore, SSLLService.All.Done);
+		}
+
+		[TestMethod]
+		public void then_the_scan_results_should_be_public()
+		{
+			Response.isPublic.Should().BeTrue();
+		}
+	}
+
+	public abstract class PositiveTests : GenericPositiveTests<Analyze>
+	{
+		public static string TestHost;
+
+		[TestMethod]
+		public void then_the_host_property_should_match_the_requested_hostname()
+		{
+			Response.host.Should().Be(TestHost);
 		}
 
 		[TestMethod]
 		public void then_the_header_status_code_should_be_200()
 		{
-			AnalyzeResponse.Header.statusCode.Should().Be(200);
+			Response.Header.statusCode.Should().Be(200);
 		}
+
+		[TestMethod]
+		public void then_the_port_should_be_that_of_a_ssl_connection()
+		{
+			Response.port.Should().Be(443);
+		}
+	}
+
+	public abstract class NegativeTests : GenericNegativeTests<Analyze>
+	{
 	}
 }
