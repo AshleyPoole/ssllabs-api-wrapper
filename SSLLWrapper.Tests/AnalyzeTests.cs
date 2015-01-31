9c457fb6143684765a988a566fa087c7d33caeaa
AshleyPoole <git@ashleypoole.co.uk>
2015-01-31 11:31:14 +0000

    Adding negative test to analyze tests

--- SSLLWrapper.Tests/AnalyzeTests.cs
+++ SSLLWrapper.Tests/AnalyzeTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SSLLWrapper;
@@ -84,14 +85,14 @@ namespace given_that_I_make_a_analyze_request
 			TestHost = "https://www.ashleypoole.co.uk";
 			var webResponseModel = new WebResponseModel()
 			{
-				Payloay = " {\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTP\",\"isPublic\":false,\"" +
+				Payloay = "{\"host\":\"www.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTP\",\"isPublic\":false,\"" +
 				          "status\":\"IN_PROGRESS\",\"startTime\":1422479488403,\"engineVersion\":\"1.12.8\",\"criteriaVersion\":\"2009i\"" +
 				          ",\"endpoints\":[{\"ipAddress\":\"104.28.6.2\",\"statusMessage\":\"In progress\",\"statusDetails\":\"TESTING_HTTPS\"" +
 				          ",\"statusDetailsMessage\":\"Sending one complete HTTPS request\",\"progress\":-1,\"eta\":-1,\"delegation\":3}," +
 				          "{\"ipAddress\":\"104.28.7.2\",\"statusMessage\":\"Pending\",\"progress\":-1,\"eta\":-1,\"delegation\":3}]}",
 				StatusCode = 200,
 				StatusDescription = "Ok",
-				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost)
+				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost + "&publish=on&clearCache=on&all=done")
 			};
 
 			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
@@ -108,6 +109,32 @@ namespace given_that_I_make_a_analyze_request
 		}
 	}
 
+	[TestClass]
+	public class when_a_invalid_request_is_made_with_all_the_inputs_and_the_scan_is_unable_to_resolve_hostname : NegativeTests
+	{
+		[ClassInitialize]
+		public static void Setup(TestContext testContext)
+		{
+			var mockedApiProvider = new Mock<IApiProvider>();
+			TestHost = "https://www.ashleypoole.co.uk";
+			var webResponseModel = new WebResponseModel()
+			{
+				Payloay = "{\"host\":\"www2.ashleypoole.co.uk\",\"port\":443,\"protocol\":\"HTTP\",\"isPublic\":false,\"status\":\"ERROR\"," +
+				          "\"statusMessage\":\"Unable to resolve domain name\",\"startTime\":1422478797953,\"testTime\":1422478798017," +
+				          "\"engineVersion\":\"1.12.8\",\"criteriaVersion\":\"2009i\",\"cacheExpiryTime\":1422478858017}",
+				StatusCode = 500,
+				StatusDescription = "Bad Request",
+				Url = ("https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=" + TestHost)
+			};
+
+			mockedApiProvider.Setup(x => x.MakeGetRequest(It.IsAny<RequestModel>())).Returns(webResponseModel);
+
+			var ssllService = new SSLLService("https://api.dev.ssllabs.com/api/fa78d5a4/", mockedApiProvider.Object);
+			Response = ssllService.Analyze(TestHost, SSLLService.Publish.On, SSLLService.ClearCache.On,
+				SSLLService.FromCache.Ignore, SSLLService.All.Done);
+		}
+	}
+
 	public abstract class PositiveTests : GenericPositiveTests<Analyze>
 	{
 		public static string TestHost;
@@ -115,7 +142,7 @@ namespace given_that_I_make_a_analyze_request
 		[TestMethod]
 		public void then_the_host_property_should_match_the_requested_hostname()
 		{
-			Response.host.Should().Be(TestHost);
+			Response.host.Should().Be(TestHost.Substring(8));
 		}
 
 		[TestMethod]
@@ -133,5 +160,18 @@ namespace given_that_I_make_a_analyze_request
 
 	public abstract class NegativeTests : GenericNegativeTests<Analyze>
 	{
+		public static string TestHost;
+
+		[TestMethod]
+		public void then_an_error_occurred_should_be_marked()
+		{
+			Response.HasErrorOccurred.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void then_a_error_message_should_be_returned()
+		{
+			Response.Errors.Count.Should().BeGreaterOrEqualTo(1);
+		}
 	}
 }
